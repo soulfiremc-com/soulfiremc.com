e6080cd2c07f1fe93a3d0cc67a718c491822f1ae
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-06-06 21:53:57 +0200

    chore: set identify url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -21,7 +21,9 @@ const clientOptions = {
     adminClient(),
     lastLoginMethodClient(),
     dashClient(),
-    sentinelClient(),
+    sentinelClient({
+      identifyUrl: import.meta.env.VITE_BETTER_AUTH_IDENTIFY_URL
+    }),
   ],
 };
 
