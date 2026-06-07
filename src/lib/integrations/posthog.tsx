55d7b33cf90c82257d778b5981feef5889468746
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-06-07 11:12:21 +0200

    chore: update posthog url

--- src/lib/integrations/posthog.tsx
+++ src/lib/integrations/posthog.tsx
@@ -5,7 +5,7 @@ const POSTHOG_KEY = import.meta.env.VITE_POSTHOG_KEY;
 
 if (typeof window !== "undefined" && POSTHOG_KEY) {
   posthog.init(POSTHOG_KEY, {
-    api_host: "https://e.soulfiremc.com",
+    api_host: "https://t.soulfiremc.com",
     ui_host: "https://eu.posthog.com",
     defaults: "2025-05-24",
     capture_exceptions: true,
