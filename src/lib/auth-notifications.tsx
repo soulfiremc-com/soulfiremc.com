045470e8e6bece8c867b17744be84f688e3115f2
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-07-25 13:39:06 +0200

    fix: update import

--- src/lib/auth-notifications.tsx
+++ src/lib/auth-notifications.tsx
@@ -10,7 +10,7 @@ import {
   Preview,
   Section,
   Text,
-} from "@react-email/components";
+} from "react-email";
 import type { ReactNode } from "react";
 import { sendEmail } from "@/lib/resend";
 
